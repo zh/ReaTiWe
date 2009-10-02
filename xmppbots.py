5748b6db9ec766880313b7b7358517445a940984
Stoyan Zhekov <zh@zhware.net>
2009-10-02 15:44:56 +0900

    direct msgs between users

--- xmppbots.py
+++ xmppbots.py
@@ -22,38 +22,69 @@ class XMPPHandler(webapp.RequestHandler):
     microUser = MicroUser.gql('WHERE jid = :1', jid).get()
     # only register users
     if not microUser:
-      message.reply("not authorized")
+      message.reply("Error: not authorized")
       return
-    logging.debug("msg from: %s" % bare_jid)
-    if message.body[0:4].lower() == u"ping":
+    msg = [x.strip() for x in message.body.split() if x != None]
+    if msg[0].lower() == u"ping":
       message.reply("PONG! :)")
-    elif message.body[0:4].lower() == u"@me " and len(message.body) > 5:
-      text = message.body[4:].strip().replace('\n','').replace('\r',' ').replace('\t',' ')
-      micro = MicroEntry( author=microUser, content=text)
-      micro.put()
-      # ping the PuSH hub (current user)
-      form_fields = { "hub.mode": "publish",
-                        "hub.url": "http://reatiwe.appspot.com/user/%s/atom" % microUser.nick }
-      response = urlfetch.fetch(url = "https://pubsubhubbub.appspot.com/",
-                        payload = urllib.urlencode(form_fields),
-                        method = urlfetch.POST,
-                        headers = {'Content-Type': 'application/x-www-form-urlencoded'})
-      message.reply("message sent #%s." % str(entityid(micro)))
-    elif message.body[0:4].lower() == u"nick":
+    elif msg[0].lower() == u"nick":
       if len(message.body) > 6:
         nick = message.body[5:].strip()
         exists = MicroUser.gql('WHERE nick = :1', nick).get()
         if exists and microUser.nick != exists.nick:
-          message.reply("Nickname not available.")
+          message.reply("Error: Nickname not available.")
         elif len(nick) > 32 or len(nick) < 4:
-          message.reply("Nickname must be between 4 and 32 characters long.")
+          message.reply("Error: Nickname must be between 4 and 32 characters long.")
         elif re.match('^[a-z][a-z0-9]*$', nick) == None:
-          message.reply("Nickname may only contain characters a-z and 0-9 (no uppercase) and must start with a-z.")
+          message.reply("Error: Nickname may only contain characters a-z and 0-9 (no uppercase) and must start with a-z.")
         else:
           microUser.nick = nick
           microUser.put()
-      message.reply("Nickname: %s" % microUser.nick)
-    # TODO: other commands - last, on, off, follow etc.  
+      message.reply("nickname: %s" % microUser.nick)
+    # TODO: other commands - last, on, off, follow etc.
+    elif msg[0].lower() == u"last":
+      text = "Last messages:\n"
+      micros = MicroEntry.all().order('-date').fetch(10)
+      micros.reverse()
+      for m in micros:
+        text += "@%s:\n%s\n#%s (%s ago) http://reatiwe.appspot.com/entry/%s\n\n" % (m.author.nick, m.content, str(entityid(m)), timestamp(m.date), str(entityid(m)))
+      message.reply(text)
     else:
-      logging.debug("unknown msg: %s" % str(self.request.get('stanza')))
-      message.reply("Error: unknown message")
+      # regular expressions, heavy stuff
+      pattern = re.compile('^@([a-z][a-z0-9]*)\s*(.*)')
+      m = pattern.match(message.body)
+      if m and m.group(2):
+        to_nick = m.group(1)
+        msg = m.group(2)
+        text = msg.strip().replace('\n','').replace('\r',' ').replace('\t',' ')
+        # if message is to myself, blog it
+        if to_nick == u"me":
+          micro = MicroEntry( author=microUser, content=text)
+          micro.put()
+          # ping the PuSH hub (current user)
+          form_fields = { "hub.mode": "publish",
+                          "hub.url": "http://reatiwe.appspot.com/user/%s/atom" % microUser.nick }
+          response = urlfetch.fetch(url = "https://pubsubhubbub.appspot.com/",
+                          payload = urllib.urlencode(form_fields),
+                          method = urlfetch.POST,
+                          headers = {'Content-Type': 'application/x-www-form-urlencoded'})
+          message.reply("message sent: http://reatiwe.appspot.com/entry/%s" % str(entityid(micro)))
+        # message to another user  
+        else:  
+          toUser = MicroUser.gql('WHERE nick = :1', to_nick).get()
+          if not toUser:
+            message.reply("Error: not implemented.")
+            return
+          else:
+            to_jid = toUser.jid.address
+            #chat_message_sent = False
+            #if xmpp.get_presence(to_jid):
+            #  status_code = xmpp.send_message(to_jid, "message from @%s:\n%s" % (bare_jid, msg))
+            xmpp.send_message(to_jid, "message from @%s:\n%s" % (microUser.nick, msg))
+            #  chat_message_sent = (status_code != xmpp.NO_ERROR)
+            #if not chat_message_sent:  
+            #  logging.debug("msg (%s -> %s) not sent." % (bare_jid, to_jid))
+            message.reply("message to %s sent." % to_nick)
+      else:  
+        logging.debug("unknown msg: %s" % str(self.request.get('stanza')))
+        message.reply("Error: not implemented.")
